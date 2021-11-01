937a3f418cc26ebfe3b1eaf38aa2618d9d1f1b23
Adwait Kesharwani <adwait1291@gmail.com>
2021-11-01 22:30:41 +0530

    Update Final_ISP_Project.py

--- Final_ISP_Project.py
+++ Final_ISP_Project.py
@@ -14,6 +14,7 @@ from nltk import pos_tag
 import nltk
 nltk.download('averaged_perceptron_tagger')
 nltk.download('stopwords')
+nltk.download('wordnet')
 stopwords = nltk.corpus.stopwords.words('english')
 from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
