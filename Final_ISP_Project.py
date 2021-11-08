a8e5c1a1c49e54c3a9700c6b7c3f3328943dc8cf
Yashashvi Singh <73705143+Yashi11@users.noreply.github.com>
2021-11-08 15:38:33 +0530

    Update Final_ISP_Project.py

--- Final_ISP_Project.py
+++ Final_ISP_Project.py
@@ -104,7 +104,16 @@ pickle_in = open("KNeighborsClassifier.pkl","rb")
 KNeighborsClassifier = pickle.load(pickle_in)
 
 #---------------HTML--------------#
+page_bg_img = '''
+<style>
+body {
+background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+background-size: cover;
+}
+</style>
+'''
 
+st.markdown(page_bg_img, unsafe_allow_html=True)
 html_temp = """
     <div style="background-color: pink;padding:10px;
     height: 100%;
