bb23e25e9ab0cd224ce89fd6f00bd2846b59d67a
Adwait Kesharwani <adwait1291@gmail.com>
2022-07-01 17:24:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ import streamlit as st
 import string
 from nltk import pos_tag
 import nltk
+
 nltk.download('averaged_perceptron_tagger')
 nltk.download('stopwords')
 nltk.download('wordnet')
@@ -24,65 +25,64 @@ from nltk.stem import WordNetLemmatizer
 
 #---------------Functions to clean the input text--------------#
 
-def tokenize_remove_punctuation(text):
-        clean_text = []
-        text = text.split(" ")
-        for word in text:
-            word = list(word)
-            new_word = []
-            for c in word:
-                if c not in string.punctuation:
-                    new_word.append(c)
-                word = "".join(new_word)
+def tokenize_remove_punctuations(text):
+    clean_text = []
+    text = text.split(" ")
+    for word in text:
+        word = list(word)
+        new_word = []
+        for c in word:
+            if c not in string.punctuation:
+                new_word.append(c)
+        word = "".join(new_word)
+        if len(word)>0:
             clean_text.append(word)
-        return clean_text
+    return clean_text
     
 def remove_stopwords(text):
-        clean_text = []
-        for word in text:
-            if word not in stopwords:
-                clean_text.append(word)
-        return clean_text
+    clean_text = []
+    for word in text:
+        if word not in stopwords:
+            clean_text.append(word)
+    return clean_text
     
 def pos_tagging(text):
-   tagged = nltk.pos_tag(text)
-   return tagged
+    tagged = nltk.pos_tag(text)
+    return tagged
             
 def get_wordnet(pos_tag):
-        if pos_tag.startswith('J'):
-            return wordnet.ADJ
-        elif pos_tag.startswith('V'):
-            return wordnet.VERB
-        elif pos_tag.startswith('N'):
-            return wordnet.NOUN
-        elif pos_tag.startswith('R'):
-            return wordnet.ADV
-        else:
-            return wordnet.NOUN
+    if pos_tag.startswith('J'):
+        return wordnet.ADJ
+    elif pos_tag.startswith('V'):
+        return wordnet.VERB
+    elif pos_tag.startswith('N'):
+        return wordnet.NOUN
+    elif pos_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN
+
+def lemmatize(pos_tags):
+    lemmatized_text = []
+    for t in pos_tags:
+        word = WordNetLemmatizer().lemmatize(t[0],get_wordnet(t[1]))
+        lemmatized_text.append(word)
+    return lemmatized_text
         
 def clean_text(text):
-        text = str(text)
-            #Converting text to lower-case
-        text = text.lower()
-            #tokenize and remove punctuations from the text
-        text = tokenize_remove_punctuation(text)
-            #remove words containing numericals
-        text = [word for word in text if not any(c.isdigit() for c in word)]
-            #remove stopwords
-        text = remove_stopwords(text)
-            #remove empty tokens
-        text = [ t for t in text if len(t) > 0]
-            #pos tagging
-        pos_tags = pos_tagging(text)
-            #Lemmatize text
-        text = [WordNetLemmatizer().lemmatize(t[0],get_wordnet(t[1])) for t in pos_tags]
-            #remove words with only one letter
-        text = [ t for t in text if len(t)>1]
-            #join all words
-        text = " ".join(text)
-        return text
+    text = str(text)
+    text = text.lower()
+    text = tokenize_remove_punctuations(text)
+    text = [word for word in text if not any(c.isdigit() for c in word)]
+    text = remove_stopwords(text)
+    text = [ t for t in text if len(t) > 0]
+    pos_tags = pos_tagging(text)
+    text = lemmatize(pos_tags)
+    text = [ t for t in text if len(t)>1]
+    text = " ".join(text)
+    return text
         
-def transform_input(self, text):
+def transform_input(text):
         text = np.array([text])
         text = pd.Series(text)
         text = clean_text(text)
